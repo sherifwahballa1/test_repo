5302be0fa50c618516c22255a3567be0c95a1933
sherifwahballa1 <sherifwahballa0@gmail.com>
2022-05-16 09:30:48 +0200

    new comments

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-// console.warn('welcome from main page');
+console.warn('welcome from main page');
 
-// console.error('welcome from main page');
+console.error('welcome from main page');
 
-// console.log('welcome from main page');
+console.log('welcome from main page');
 
 module.exports = app;
